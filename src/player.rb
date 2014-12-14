0aa472ffcde237e997aa899f28b2cfa09e11b719
Jorman Bustos <tille100@gmail.com>
2014-12-14 08:43:56 -0500

    Variable constraints.

--- src/player.rb
+++ src/player.rb
@@ -1,19 +1,20 @@
 class Player
-  attr_reader :score, :snake
+  attr_accessor :score, :snake, :window
+  private :window, :snake
 
   VELOCITY = 20
 
   def initialize(window)
-    @score = 0
-    @snake = Array.new
-    @window = window
+    self.score = 0
+    self.snake = Array.new
+    self.window = window
     initialize_snake
   end
 
   def initialize_snake
-    block_1 = Block.new(@window, 320, 240)
-    block_2 = Block.new(@window, 300, 240)
-    block_3 = Block.new(@window, 280, 240)
+    block_1 = Block.new(window, 320, 240)
+    block_2 = Block.new(window, 300, 240)
+    block_3 = Block.new(window, 280, 240)
     snake.push(block_1, block_2, block_3)
   end
 
