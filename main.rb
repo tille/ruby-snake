a3ebd84d82df32674b5035fce8bfbbe5cd7dfaff
Jorman Bustos <tille100@gmail.com>
2014-12-13 20:34:26 -0500

    Remove unused score font.

--- main.rb
+++ main.rb
@@ -6,7 +6,6 @@ class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
     self.caption = "Ruby Snake by T!ll3"
-    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @player = Player.new(self)
   end
 
