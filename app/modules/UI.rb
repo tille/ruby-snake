10314631653544329cc4f39b12f068488e344cf5
Jorman Bustos <tille100@gmail.com>
2014-12-21 11:24:53 -0500

    Use local vars instead accessor methods.

--- app/modules/UI.rb
+++ app/modules/UI.rb
@@ -7,16 +7,16 @@ module UI
   end
 
   public
-    def draw_score(window, player)
-      score_msg = "Score: #{player.score}"
+    def draw_score(window)
+      score_msg = "Score: #{@player.score}"
       font = default_font(window)
       font.draw(score_msg, x_pos = 10, y_pos = 10,
                 z_pos = 1, 1.0, 1.0, 0xffffff00)
     end
 
-    def game_over(window, player)
-      player.gap = 1000000
-      player.game_over = true
+    def game_over(window)
+      @player.gap = 2**30 # infinity
+      @player.game_over = true
       message = "Game Over"
       default_font(window).draw(message,
         x_pos = 640/2.0,
--- main.rb
+++ main.rb
@@ -4,28 +4,27 @@ require 'require_all'
 require_all 'app'
 
 class GameWindow < Gosu::Window
-  attr_accessor :player, :target
   include Keyboard
   include UI
 
   def initialize
     super(640, 480, false)
     self.caption = "Ruby Snake by T!ll3"
-    self.player = Player.new(self)
-    self.target = Block.new(self)
+    @player = Player.new(self)
+    @target = Block.new(self)
   end
 
   def update
     handle_keys
-    player.auto_move
+    @player.auto_move
   end
 
   def draw
-    player.collision(target)
-    game_over(self, player) if player.collision_snake?
-    draw_score(self, player)
-    target.draw
-    player.draw
+    @player.collision(@target)
+    game_over(self) if @player.collision_snake?
+    draw_score(self)
+    @target.draw
+    @player.draw
   end
 end
 
